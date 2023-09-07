645e5c89e649fc9434282d470533fc873e05b266
Cameron James <cameron@flymachine.com>
2023-09-07 15:18:15 -0700

    Show deck 3 by default for ship/icon view; fix bug hiding rooms on iniital page load

--- ui/src/ship/ShipAspectTrait.ts
+++ ui/src/ship/ShipAspectTrait.ts
@@ -1,11 +1,11 @@
 // Copyright 2015-2022 Swim.inc
 // All rights reserved.
 
-import {Lazy} from "@swim/util";
-import type {Graphics} from "@swim/graphics";
-import {VectorIcon} from "@swim/graphics";
-import type {SheetController} from "@swim/sheet";
-import {AspectTrait} from "@swim/domain";
+import { Lazy } from "@swim/util";
+import type { Graphics } from "@swim/graphics";
+import { VectorIcon } from "@swim/graphics";
+import type { SheetController } from "@swim/sheet";
+import { AspectTrait } from "@swim/domain";
 import { DeckBoardController } from "../deck";
 
 /** @public */
@@ -20,12 +20,18 @@ export class ShipAspectTrait extends AspectTrait {
   // Define the board controller to be used that will control all the widgets/cards the ship will have
   // We will use the 'DeckBoardController' which is 2 panels showing the statuses of a deck's rooms
   override createTabController(): SheetController | null {
-    return new DeckBoardController();
+    return new DeckBoardController().set({
+      nodeUri: "/ship/icon/deck/3",
+    });
   }
 
   // The icon to show the 'portal' (at the top - alternative to atlas)
   @Lazy
   static get icon(): Graphics {
-    return VectorIcon.create(24, 24, "M19 5v2h-4V5h4M9 5v6H5V5h4m10 8v6h-4v-6h4M9 17v2H5v-2h4M21 3h-8v6h8V3zM11 3H3v10h8V3zm10 8h-8v10h8V11zm-10 4H3v6h8v-6z");
+    return VectorIcon.create(
+      24,
+      24,
+      "M19 5v2h-4V5h4M9 5v6H5V5h4m10 8v6h-4v-6h4M9 17v2H5v-2h4M21 3h-8v6h8V3zM11 3H3v10h8V3zm10 8h-8v10h8V11zm-10 4H3v6h8v-6z"
+    );
   }
 }
